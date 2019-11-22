e65106ad4444a6b8936ca3957ad313d8143b6352
mori <mori>
2019-11-21 21:42:44 -0800

    new features

--- c2.py
+++ c2.py
@@ -7,6 +7,7 @@ import logging
 import telegram
 import time
 import shlex
+import apt
 
 if os.getuid() != 0:
     print("please run as root...")
@@ -38,15 +39,60 @@ def sendTelegram(result):
     chat_id = bot.get_updates()[-1].message.chat_id
     bot.send_message(chat_id=chat_id, text=string)
 
+#badly done need to fix
+def install_package(package_name, chat_id):
+    cache = apt.cache.Cache()
+    cache.update()
+    cache.open()
+    try:
+        pkg = cache[package_name]
+    except:
+        text = "package not found..."
+        bot.send_message(chat_id=chat_id, text=text)
+        return
+        
+    if pkg.is_installed:
+        text = "package is already installed..."
+        bot.send_message(chat_id=chat_id, text=text)
+    else:
+        pkg.mark_install()
+        try:
+            cache.commit()
+            text = "package " + package_name + " installed..."
+            bot.send_message(chat_id=chat_id, text=text)
+        except:
+            text = "error: check spelling"
+            bot.send_message(chat_id=chat_id, text=text)
 
 def main():
     print("waiting for a text message...")
     resultstr = WaitGetHost()
     resultstr = shlex.split(resultstr)
     #resultstr.split()
-    chat_id = bot.get_updates()[-1].message.chat_id
-    resultstr.insert(0, 'proxychains')
+
+    #default insert tor proxy
+    if not 'np' in resultstr:
+        resultstr.insert(0, 'proxychains')
     
+    #get rid of no-proxy in commands
+    elif 'np' in resultstr:
+        resultstr.pop(0)
+
+    if 'install' in resultstr:
+        install_in = resultstr.index('install')
+        pkg_inst = resultstr[install_in + 1]
+        chat_id = bot.get_updates()[-1].message.chat_id
+        output = "installing package ..." + pkg_inst
+        bot.send_message(chat_id=chat_id, text=output)
+        chat_id = bot.get_updates()[-1].message.chat_id
+        install_package(pkg_inst, chat_id)
+        main()
+     
+
+    #fetch latest human chat
+    chat_id = bot.get_updates()[-1].message.chat_id
+    
+    #send command
     result = subprocess.run(resultstr, stdout=subprocess.PIPE)
     output = str(result.stdout)
     output = output[:1500]
